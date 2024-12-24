a3a87b92f784e3a832f876b91a2ff83d113f67e8
Yuri Ramos <yurigabriel21@yahoo.com>
2024-12-24 01:10:02 -0300

    feat: create controller and method of creation a new user

---
+++ src/main/java/tech/yuri/agregadorinvestimentos/controller/UserController.java
@@ -0,0 +1,26 @@
+package tech.yuri.agregadorinvestimentos.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.yuri.agregadorinvestimentos.dto.CreateUserDto;
+import tech.yuri.agregadorinvestimentos.entity.User;
+import tech.yuri.agregadorinvestimentos.service.UserService;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDto data){
+        var userId = userService.createUser(data);
+
+        return ResponseEntity.created(URI.create("/users/"+ userId.toString())).build();
+    }
+}
